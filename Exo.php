c7da929f96821f24ecace60051fa1ad95d094630
Bairiie <lvntmarine@gmail.com>
2024-07-26 14:51:45 +0200

    ETAPE 1 & 2

---
+++ Exo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+/* -------------------------------- */
+
+echo "LA GESTION DU BUDGET" . "\n";
+
+/* -------------------------------- */
+
+$budget = readfile("Quel est le budget ? ") . "\n";
+
+if ($nombre > 0) {
+    echo "Merci pour votre réponse.";
+    }
+
+    else {
+        echo "Veuillez indiquer votre budget.";
+    }
+?>
+</body>
+</html>
